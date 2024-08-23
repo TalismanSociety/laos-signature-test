2fdff3a49f2ba8165034a51004adf57ed934be11
alecdwm <alec@owls.io>
2024-08-23 12:56:45 +0000

    added tailwind and @polkadot/api

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
+const presetWsUrls = [
+  "wss://rpc.laos.laosfoundation.io",
+  "wss://rpc.laosmercury.gorengine.com",
+  "wss://wss.api.moonbeam.network",
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [wsUrl, setWsUrl] = useState(presetWsUrls[0])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <label htmlFor="wsurl">ws url</label>
+      <input
+        id="wsurl"
+        type="text"
+        value={wsUrl}
+        onChange={(e) => setWsUrl(e.currentTarget.value)}
+      />
+      <div></div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        {/* <button onClick={() => setCount((count) => count + 1)}>count is {count}</button> */}
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   )
 }
